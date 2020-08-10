55049dd192a12831e2d7b4b654958b77e10506eb
Shine941 <68743133+Shine941@users.noreply.github.com>
2020-08-10 12:51:03 +0800

    Update hello.go

--- github.com/01/hello/hello.go
+++ github.com/01/hello/hello.go
@@ -12,11 +12,11 @@ func main() {
 	fmt.Println("请输入h或其他")
 	fmt.Scanln(&x)
 	unity.Choose(x)
-	fmt.Println("请输入type的值：0或1")
+	fmt.Println("请输入type的值：1或2")
 	fmt.Scanln(&ty)
 	if ty == 1 {
 		fmt.Println("春眠不觉晓")
-	} else if ty == 0 {
+	} else if ty == 2 {
 		fmt.Println("似花还似非花,也无人惜从教坠")
 	} else {
 		fmt.Println("输入错误")
